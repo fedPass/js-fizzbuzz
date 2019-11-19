bf1d92eb83d95ab36f1cf880b08e5f63aaa01fda
fedPass <federica.passanante@gmail.com>
2019-11-19 14:46:46 +0100

    index e main

---
+++ main.js
@@ -0,0 +1,14 @@
+// multipli di 3 stampare "Fizz"
+// multipli di 5 stampare "Buzz"
+// multipli di 3 e 5 stampare "FizzBuzz"
+
+// Stampare tutti i numeri da 1 a 100
+ for (var i = 1; i <= 100; i++) {
+     if ([i] % 3 == 0) {
+         console.log('Fizz');
+     } else if ([i] % 5 == 0) {
+         console.log('Buzz');
+     }  else {
+        console.log([i]);
+     }
+ }
