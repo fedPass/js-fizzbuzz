6f3cc1f6fc6c0dd996dca70d3f47ffca57e585b2
fedPass <federica.passanante@gmail.com>
2019-11-19 16:28:31 +0100

    soluzione multi if

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
 
 
-// multipli di 3 e 5 stampare "FizzBuzz"
+
 
 // Stampare tutti i numeri da 1 a 100
- // for (var i = 1; i <= 100; i++) {
- //     // multipli di 3 stampare "Fizz"
- //     if ([i] % 3 == 0) {
- //         console.log('Fizz');
- //     } else if ([i] % 5 == 0)  {
- //     // multipli di 5 stampare "Buzz"
- //         console.log('Buzz');
- //     } else {
- //        console.log([i]);
- //     }
- // }
-
- for (var i = 1; i <= 100; i++) {
+  for (var i = 1; i <= 100; i++) {
      // multipli di 3 stampare "Fizz"
      if ([i] % 3 == 0) {
          console.log('Fizz');
@@ -27,12 +15,12 @@
      }
 
      if (([i] % 5 == 0) && ([i] % 3 == 0))  {
-      // multipli di 15 stampare "Buzz"
+     // multipli di 3 e 5 stampare "FizzBuzz"
           console.log('FizzBuzz');
       }
 
      if (([i] % 5 != 0) && ([i] % 3 != 0))  {
-      // multipli di 15 stampare "Buzz"
+      // numero che non soddisfa le condizioni prima
           console.log([i]);
       }
  }
