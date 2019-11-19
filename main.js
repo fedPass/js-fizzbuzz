b0a678ce6d44c04505175120d0d57a2c57b4d3bb
fedPass <federica.passanante@gmail.com>
2019-11-19 17:07:16 +0100

    prova altre soluzioni

--- main.js
+++ main.js
@@ -1,26 +1,60 @@
+//soluzione multi if
+ //  for (var i = 1; i <= 100; i++) {
+ //     // multipli di 3 stampare "Fizz"
+ //     if ([i] % 3 == 0) {
+ //         console.log('Fizz');
+ //     }
+ //     // multipli di 5 stampare "Buzz"
+ //     if ([i] % 5 == 0)  {
+ //         console.log('Buzz');
+ //     }
+ //     // multipli di 3 e 5 stampare "FizzBuzz"
+ //     if (([i] % 5 == 0) && ([i] % 3 == 0))  {
+ //          console.log('FizzBuzz');
+ //      }
+ //     // numero che non soddisfa le condizioni prima
+ //     if (([i] % 5 != 0) && ([i] % 3 != 0))  {
+ //          console.log([i]);
+ //      }
+ // }
 
-
-
-
-// Stampare tutti i numeri da 1 a 100
-  for (var i = 1; i <= 100; i++) {
-     // multipli di 3 stampare "Fizz"
-     if ([i] % 3 == 0) {
-         console.log('Fizz');
-     }
-
-     if ([i] % 5 == 0)  {
-     // multipli di 5 stampare "Buzz"
+ // soluzione if else if - else
+ for (var i = 1; i <= 100; i++) {
+    // multipli di 3 stampare "Fizz"
+    if ([i] % 3 == 0) {
+        console.log('Fizz');
+        if (([i] % 3 == 0) && ([i] % 5 == 0))  {
+        // multipli di 5 stampare "Buzz"
+            console.log('FizzBuzz');
+        }
+    } else if ([i] % 5 == 0)  {
+    // multipli di 3 e 5 stampare "FizzBuzz"
          console.log('Buzz');
+     } else {
+     // numero che non soddisfa le condizioni prima
+         console.log([i]);
      }
+}
 
-     if (([i] % 5 == 0) && ([i] % 3 == 0))  {
-     // multipli di 3 e 5 stampare "FizzBuzz"
-          console.log('FizzBuzz');
-      }
-
-     if (([i] % 5 != 0) && ([i] % 3 != 0))  {
-      // numero che non soddisfa le condizioni prima
-          console.log([i]);
-      }
- }
+ // prova con switch
+// for (var i = 1; i <= 99; i++) {
+//     console.log([i]);
+// }
+//
+// switch ([i]) {
+//    case "1" :
+//       [i] % 3 == 0;
+//       console.log('Fizz');
+//       break;
+//   case "2" :
+//     [i] % 5 == 0;
+//     console.log('Buzz');
+//     break;
+//   case "3":
+//     ([i] % 5 != 0) && ([i] % 3 != 0);
+//     console.log('FizzBuzz');
+//     break;
+//   default:
+//     console.log([i]);
+//     break;
+// }
