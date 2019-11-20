94232023b2da5b01e79b67decd385b349c9d42fc
fedPass <federica.passanante@gmail.com>
2019-11-20 09:30:25 +0100

    es con switch

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 //soluzione multi if
- //  for (var i = 1; i <= 100; i++) {
- //     // multipli di 3 stampare "Fizz"
- //     if ([i] % 3 == 0) {
- //         console.log('Fizz');
- //     }
- //     // multipli di 5 stampare "Buzz"
- //     if ([i] % 5 == 0)  {
- //         console.log('Buzz');
- //     }
- //     // multipli di 3 e 5 stampare "FizzBuzz"
- //     if (([i] % 5 == 0) && ([i] % 3 == 0))  {
- //          console.log('FizzBuzz');
- //      }
- //     // numero che non soddisfa le condizioni prima
- //     if (([i] % 5 != 0) && ([i] % 3 != 0))  {
- //          console.log([i]);
- //      }
- // }
+  for (var i = 1; i <= 100; i++) {
+     // multipli di 3 stampare "Fizz"
+     if ([i] % 3 == 0) {
+         console.log('Fizz');
+     }
+     // multipli di 5 stampare "Buzz"
+     if ([i] % 5 == 0)  {
+         console.log('Buzz');
+     }
+     // multipli di 3 e 5 stampare "FizzBuzz"
+     if (([i] % 5 == 0) && ([i] % 3 == 0))  {
+          console.log('FizzBuzz');
+      }
+     // numero che non soddisfa le condizioni prima
+     if (([i] % 5 != 0) && ([i] % 3 != 0))  {
+          console.log([i]);
+      }
+ }
 
  // soluzione if - else if - else
  for (var i = 1; i <= 100; i++) {
@@ -56,3 +56,22 @@
 //         break;
 //     }
 // }
+
+
+// prova con switch
+for (var x = 1; x <= 100; x++) {
+  switch (true) {
+    case (x % 5 === 0 && x % 3 === 0):
+        console.log("FizzBuzz");
+        break;
+    case x % 3 === 0:
+        console.log("Fizz");
+        break;
+    case x % 5 === 0:
+        console.log("Buzz");
+        break;
+    default:
+        console.log(x);
+        break;
+  }
+}
